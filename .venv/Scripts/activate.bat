67c5986bc50207bc565472e8afba3a6bf78ca7e6
Samuel Sameh <samuellsameh611@gmail.com>
2024-09-11 18:35:09 +0300

    update

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=d:\Sam's Projects\pig-project\.venv
+set VIRTUAL_ENV=d:\Sam's Projects\mini-projects\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
